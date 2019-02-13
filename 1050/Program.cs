d4c569e929c4a61f92189c11fb1d168d90b45778
Otávio Araújo <otavio-luiz17@outlook.com>
2019-02-12 23:41:29 -0200

    Projeto DDD

---
+++ 1050/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string cidade;
+            x = int.Parse(Console.ReadLine());
+
+            switch(x)
+            {
+                case 61:
+                    cidade = "Brasilia";
+                    break;
+                case 71:
+                    cidade = "Salvador";
+                    break;
+                case 11:
+                    cidade = "Sao Paulo";
+                    break;
+                case 21:
+                    cidade = "Rio de Janeiro";
+                    break;
+                case 32:
+                    cidade = "Juiz de Fora";
+                    break;
+                case 19:
+                    cidade = "Campinas";
+                    break;
+                case 27:
+                    cidade = "Vitoria";
+                    break;
+                case 31:
+                    cidade = "Belo Horizonte";
+                    break;
+                default:
+                    cidade = "DDD NAO CADASTRADO";
+                    break;
+                    
+
+
+            }
+            Console.WriteLine(cidade);
+            Console.ReadLine();
+        }
+    }
+}
